b42c7b89c060ae4300c4f2de722aaa8dccafb07e
James Mulligan <james.mulligan@berkeley.edu>
2023-05-27 18:05:34 -0700

    update init scripts

--- init_hiccup.sh
+++ init_hiccup.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/bash
 
-# Set up pyenv (for python version management)
-export PYENV_ROOT="/home/software/users/james/pyenv"
-export PYTHON_CONFIGURE_OPTS="--enable-shared"
-export PATH="${PATH}:${PYENV_ROOT}/bin"
-eval "$(pyenv init --path)"
-eval "$(pyenv init -)"
-pyenv local 3.9.12
+if [[ "$PATH" == *".pyenv"* ]]; then
+    # Set up pyenv (for python version management)
+    export PYENV_ROOT="/home/software/users/james/pyenv"
+    export PYTHON_CONFIGURE_OPTS="--enable-shared"
+    export PATH="${PATH}:${PYENV_ROOT}/bin"
+    eval "$(pyenv init --path)"
+    eval "$(pyenv init -)"
+    pyenv local 3.9.12
+fi
 
 # Get command line option to determine whether we need to install the virtual environment, or just enter it
 for i in "$@"; do
@@ -26,15 +28,20 @@ done
 if [ ! -z ${INSTALL} ]; then
     echo
     echo "Create new virtual environment..."
-    python -m venv venv
     if lspci | grep -i 'nvidia' > /dev/null; then
+        python -m venv venv_gpu
         echo "Nvidia GPU detected...installing requirements_gpu.txt"
         pip install -r requirements_gpu.txt
     else
+        python -m venv venv_cpu
         echo "No Nvidia GPU detected...installing requirements_cpu.txt"
         pip install -r requirements_cpu.txt
     fi
 fi
 
 # Initialize python virtual environment for package management
-source venv/bin/activate
+if lspci | grep -i 'nvidia' > /dev/null; then
+    source venv_gpu/bin/activate
+else
+    source venv_cpu/bin/activate
+fi
