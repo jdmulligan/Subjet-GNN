86cdb8b362b2ae2c720568a505c65690106c7a96
Dimitrios Athanasakos <DimAthanasakos>
2023-05-31 08:54:07 -0700

    added numba

--- init_perlmutter.sh
+++ init_perlmutter.sh
@@ -9,16 +9,17 @@ module list
 #   so that you will always have your custom packages every time you load that module.
 pip install --user \
 seaborn==0.11.2 \
-silx==1.1.2
+silx==1.1.2 \
+numba==0.54.1
 
 # The following packages are already installed by the pytorch module
-#matplotlib==3.5.1 \
-#networkx==2.7.1 \
-#numpy==1.21.2 \
-#pandas==1.4.1 \
-#pyyaml==6.0 \
-#scikit-learn==1.0.2 \
-#torch==1.11 \
-#torch-geometric==2.0.4 \
-#torch-scatter==2.0.9 \
-#torch-sparse==0.6.13
+# matplotlib==3.5.1 \
+# networkx==2.7.1 \
+# numpy==1.21.2 \
+# pandas==1.4.1 \
+# pyyaml==6.0 \
+# scikit-learn==1.0.2 \
+# torch==1.11 \
+# torch-geometric==2.0.4 \
+# torch-scatter==2.0.9 \
+# torch-sparse==0.6.13
